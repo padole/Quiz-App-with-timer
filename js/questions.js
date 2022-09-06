09125df1e89d00862233fc8bba3c9b4fc9892dbb
padole <inas4wealth@gmail.com>
2022-09-06 19:28:05 +0100

    fifth commit

--- js/questions.js
+++ js/questions.js
@@ -58,15 +58,117 @@ let questions = [
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+     {
+    numb: 6,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+     {
+    numb: 7,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+     {
+    numb: 8,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+     {
+    numb: 9,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+     {
+    numb: 10,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+     {
+    numb: 11,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+     {
+    numb: 12,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+     {
+    numb: 13,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+
+     {
+    numb: 14,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+     {
+    numb: 15,
+    question: "What does DOM stand for",
+    answer: "Document Object Model",
+    options: [
+       "Documentary Object Model",
+       "Document Object Motion",
+       "Document Object Model",
+      "All of the Above"
+     ]
+  },
+
+
 ];
