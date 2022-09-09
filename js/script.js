a39e09a1088ff9152db1ec63c7bf634121fe6673
padole <inas4wealth@gmail.com>
2022-09-09 10:45:02 +0100

    eighth commit

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ exit_btn.onclick = ()=>{
 
 // if continueQuiz button clicked
 continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
+    //info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.add("activeQuiz"); //show quiz box
     showQuetions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
